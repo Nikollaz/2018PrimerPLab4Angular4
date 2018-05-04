a51703360e4761c8dcfa0fd72b190a85eec26b20
Nicolas Saavedra <njsr_27@hotmail.com>
2018-05-04 18:46:22 -0300

    Se sube practica completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MiHttpService } from './servicios/mi-http/mi-http.service';
+import { PersonasService } from './servicios/personas.service';
+import { ListadoPersonasComponent } from './componentes/listado-personas/listado-personas.component';
+import { MostrarPersonaComponent } from './componentes/mostrar-persona/mostrar-persona.component'; 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoPersonasComponent,
+    MostrarPersonaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MiHttpService, PersonasService],
+  bootstrap: [AppComponent, ]
 })
 export class AppModule { }
